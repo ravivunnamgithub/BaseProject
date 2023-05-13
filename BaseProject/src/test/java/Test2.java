0d475915ac54cb4c0c8c830311cc0ce961dd6616
ravivunnamgithub <132743013+ravivunnamgithub@users.noreply.github.com>
2023-05-13 18:37:21 +0530

    Update Test2.java

--- BaseProject/src/test/java/Test2.java
+++ BaseProject/src/test/java/Test2.java
@@ -2,10 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Code from GitHub-1");
         System.out.println("Code from IntelleJ-1");
-        System.out.println("Code from GitHub-2");
-        System.out.println("Code from IntelleJ-2");
-        System.out.println("Code from GitHub-3");
-        System.out.println("Code from IntelleJ-3");
         for(int i = 5; i >= 1; i--) {
             for(int j=1;j<=10;j++) {
                 System.out.println(i+" * "+ j+" = "+i * j);

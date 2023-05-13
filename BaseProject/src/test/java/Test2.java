686d2c288796a0ed6be43f71bde18bf0f0249a84
ravivunnamgithub <132743013+ravivunnamgithub@users.noreply.github.com>
2023-05-13 18:22:58 +0530

    Update Test2.java
    Update from github

--- BaseProject/src/test/java/Test2.java
+++ BaseProject/src/test/java/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
+        System.out.println("Code from GitHub");
         for(int i = 5; i >= 1; i--) {
             for(int j=1;j<=10;j++) {
                 System.out.println(i+" * "+ j+" = "+i * j);

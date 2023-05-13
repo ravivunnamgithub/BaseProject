396fbacd4577abeb208ce288f48e1cc9ae07b957
vunnam.ravikumar <RaviKumar@RV-Laptop>
2023-05-13 18:31:27 +0530

    Commit 3

--- BaseProject/src/test/java/Test2.java
+++ BaseProject/src/test/java/Test2.java
@@ -2,7 +2,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Code from GitHub-1");
         System.out.println("Code from IntelleJ-1");
-        ystem.out.println("Code from GitHub-2");
+        System.out.println("Code from GitHub-2");
         for(int i = 5; i >= 1; i--) {
             for(int j=1;j<=10;j++) {
                 System.out.println(i+" * "+ j+" = "+i * j);
